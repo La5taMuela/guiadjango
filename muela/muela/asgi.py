f815d0c96d7635c5d7b387ae6cfc2690443ffa16
La5taMuela <joaquin1matias@gmail.com>
2024-09-02 15:23:59 -0400

    muela

---
+++ muela/muela/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for muela project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'muela.settings')
+
+application = get_asgi_application()
